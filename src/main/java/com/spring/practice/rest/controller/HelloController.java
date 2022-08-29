831dc1b3cd034621a86a75294820bc858819f696
jamie <rmk1075@gmail.com>
2022-08-29 15:17:22 +0900

    add comment - RestController, ResponseBody 설명 추가 - Controller 설명 수정

--- src/main/java/com/spring/practice/rest/controller/HelloController.java
+++ src/main/java/com/spring/practice/rest/controller/HelloController.java
@@ -17,15 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Controller 는 @Component 어노테이션을 가지고 있기 때문에 자동으로 bean 으로 등록된다.
  * @Controller 아래에서 @RequestMapping 을 사용할 수 있는데, 이를 통해 사용자의 요청과 연결되는 엔드 포인트가 된다.
  * 
- * [Spring 동작 순서]
- * 1. 사용자가 URI 형식으로 웹 서비스에 요청을 보낸다.
+ * 
+ * [Spring MVC Controller 의 요청 처리 동작 순서]
+ * 
+ * 1. Client 가 URI 형식으로 웹 서비스에 요청을 보낸다.
  * 2. DispatcherServlet 은 HandlerMapping 을 통해 요청을 처리할 메서드 (Controller) 를 찾는다.
  * 3. HandlerAdapter 를 통해 HandlerMapping 에서 찾은 Controller 에게 처리를 요청한다.
- * 4. Contoller 는 요청을 처리한 후에 HandlerAdapter 에게 결과를 반환한다.
- * 5. HandlerAdapter 가 전달받은 결과를 ModelAndView 객체로 DispatcherServlet 에게 전달한다.
- * 6. DispatcherServlet 은 전달받은 ViewResolver 를 통해 ModelAndView 객체와 매핑되는 view 를 찾는다..
- * 7. ViewResolver 처리 결과가 포함된 View 를 DispatcherServlet 에 전달한다.
- * 8. DispatcherServlet 에서 최종 응답 결과를 사용자에게 전달한다.
+ * 4. Contoller 는 요청을 처리하여 Model 객체에 적용하고 HandlerAdapter 에게 view 이름을 결과로 반환한다.
+ * 5. HandlerAdapter 가 Model 객체와 전달받은 view 이름을 통해 ModelAndView 객체로 DispatcherServlet 에게 전달한다.
+ * 6. DispatcherServlet 은 전달받은 값을 가지고 ViewResolver 에 해당 값에 매핑되는 view 가 있는지 확인한다.
+ * 7. ViewResolver 의 처리 결과가 포함된 view 를 DispatcherServlet 에 전달한다.
+ * 8. DispatcherServlet 에서 최종 응답 결과를 Client 에게 전달한다.
  */
 @Controller
 @RequestMapping("/")
--- src/main/java/com/spring/practice/rest/controller/RestHelloController.java
+++ src/main/java/com/spring/practice/rest/controller/RestHelloController.java
@@ -12,7 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @RestController
  * 
  * @RestController 는 간단하게 @Controller 와 @ResponseBody 를 더한 어노테이션이다.
+ * 매핑된 요청이 들어왔을 때, 응답으로 객체를 Response Body 에 포함시켜 Serialize 하여 전달한다.
  * 
+ * 
+ * @ResponseBody
+ * 
+ * Spring MVC 의 Controller 가 결과값을 view 로 변환하여 전달하는 것이 아니라 데이터로 전달하는 경우에 사용하는 어노테이션이다.
+ * @ResponseBody 어노테이션을 붙인 함수는 결과값을 JSON 형태로 response body 에 포함시켜서 반환한다.
+ * 
+ * 
+ * [RestController 의 요청 처리 순서]
+ * 
+ * 1. Client 가 URI 형식으로 웹 서비스에 요청을 보낸다.
+ * 2. DispatcherServlet 은 HandlerMapping 을 통해 요청을 처리할 메서드 (Controller) 를 찾는다.
+ * 3. HandlerAdapter 를 통해 HandlerMapping 에서 찾은 Controller 에게 처리를 요청한다.
+ * 4. Contoller 는 요청을 처리한 후에 HandlerAdapter 에게 결과를 반환한다. 이때 @Controller 처럼 view 이름을 반환하지 않고 결과 객체를 ResponseEntity 로 감싸서 전달한다.
+ * 5. HandlerAdapter 는 ViewResolver 를 거치지 않고 HttpMessageConverter 를 통하여 ResponseEntity 를 Http Response 의 Body 에 추가하여 결과를 변환한다.
+ * 6. HandlerAdapter 가 변환된 Http Response 를 DispatcherServlet 에게 전달한다.
+ * 7. DispatcherServlet 은 전달받은 값을 HTTP Response 로 Client 에게 전달한다.
  */
 @RestController
 @RequestMapping("/rest")
