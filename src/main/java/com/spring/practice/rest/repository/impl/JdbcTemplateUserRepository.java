8ce9e8d5b57697986ea6d8bbe76f1999567f81fa
jamie <rmk1075@gmail.com>
2022-09-27 13:50:51 +0900

    apply JpaUserRepository - change User Repository - modify some bugs

--- src/main/java/com/spring/practice/rest/repository/impl/JdbcTemplateUserRepository.java
+++ src/main/java/com/spring/practice/rest/repository/impl/JdbcTemplateUserRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -46,7 +47,7 @@ public class JdbcTemplateUserRepository implements UserRepository {
         try {
             user = jdbcTemplate.queryForObject(sql, userRowMapper());
         } catch(Exception e) {
-            throw new IllegalStateException(e);
+            if(!EmptyResultDataAccessException.class.isInstance(e)) throw new IllegalStateException(e);
         }
         return user;
     }
--- src/main/java/com/spring/practice/rest/service/impl/UserServiceImpl.java
+++ src/main/java/com/spring/practice/rest/service/impl/UserServiceImpl.java
@@ -5,18 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.practice.rest.domain.User;
 import com.spring.practice.rest.repository.UserRepository;
 import com.spring.practice.rest.service.UserService;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     @Autowired
     // @Qualifier("JdbcTemplateUserRepository") // 중복된 타입의 bean 이 존재하는 경우 @Qualifier 어노테이션을 통해서 사용할 bean name 을 지정할 수 있다.
     // @Qualifier("MockUserRepository")
-    @Qualifier("JdbcUserRepository")
+    // @Qualifier("JdbcUserRepository")
+    @Qualifier("JpaUserRepository")
     private UserRepository userRepository;
 
     @Override
