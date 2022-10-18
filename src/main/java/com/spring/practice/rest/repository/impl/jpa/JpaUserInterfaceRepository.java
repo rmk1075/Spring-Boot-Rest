f50c596fd056d7f09e189de6ec3d43d92f429c28
jamie <rmk1075@gmail.com>
2022-10-18 14:58:17 +0900

    apply Optinal

--- src/main/java/com/spring/practice/rest/repository/impl/jpa/JpaUserInterfaceRepository.java
+++ src/main/java/com/spring/practice/rest/repository/impl/jpa/JpaUserInterfaceRepository.java
@@ -1,5 +1,7 @@
 package com.spring.practice.rest.repository.impl.jpa;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.spring.practice.rest.domain.User;
 
 @Repository("JpaUserInterfaceRepository")
 public interface JpaUserInterfaceRepository extends JpaRepository<User, Long> {
-    User findById(String id);
+    Optional<User> findById(String id);
 }
