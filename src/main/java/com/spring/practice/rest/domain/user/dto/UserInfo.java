4818ab70e8884579491d0a535fea2132b71f7892
jamie <rmk1075@gmail.com>
2022-10-31 22:31:07 +0900

    add dto - UserInfo, UserCreate, UserUpdate

---
+++ src/main/java/com/spring/practice/rest/domain/user/dto/UserInfo.java
@@ -0,0 +1,24 @@
+package com.spring.practice.rest.domain.user.dto;
+
+public class UserInfo {
+    
+    // private Long id;
+    private String uid;
+    private String name;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/spring/practice/rest/domain/user/dto/UserUpdate.java
@@ -0,0 +1,14 @@
+package com.spring.practice.rest.domain.user.dto;
+
+public class UserUpdate {
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
