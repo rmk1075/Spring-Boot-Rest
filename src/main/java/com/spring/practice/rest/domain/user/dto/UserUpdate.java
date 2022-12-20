0e4289b13d9db59df90401a0d5e878cefb25908f
jamie <rmk1075@gmail.com>
2022-12-21 00:02:08 +0900

    fix: add NoArgsConstructor for jackson mapper

--- src/main/java/com/spring/practice/rest/domain/user/dto/UserUpdate.java
+++ src/main/java/com/spring/practice/rest/domain/user/dto/UserUpdate.java
@@ -1,8 +1,10 @@
 package com.spring.practice.rest.domain.user.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UserUpdate {
     
     private String name;
