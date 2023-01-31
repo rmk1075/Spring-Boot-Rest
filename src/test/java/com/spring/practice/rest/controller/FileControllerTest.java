41c8c3468fbca491b28c77047451d201d34232b8
jamie <rmk1075@gmail.com>
2023-02-01 00:58:42 +0900

    test: fix test code - FileControllerTest

--- src/test/java/com/spring/practice/rest/controller/FileControllerTest.java
+++ src/test/java/com/spring/practice/rest/controller/FileControllerTest.java
@@ -1,12 +1,16 @@
 package com.spring.practice.rest.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
@@ -16,8 +20,11 @@ public class FileControllerTest {
     FileController fileController;
 
     @Test
-    void testUploadFile() {
-        assertThrows(ResponseStatusException.class, () -> fileController.uploadFile(null));
+    void testUploadFile() throws IOException {
+        assertThrows(NullPointerException.class, () -> fileController.uploadFile(null));
+        
+        List<String> result = fileController.uploadFile(new MultipartFile[0]);
+        assertEquals(result.size(), 0);
     }
 
 }
