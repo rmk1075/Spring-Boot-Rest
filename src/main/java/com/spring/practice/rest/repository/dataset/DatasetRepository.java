f0750649a7d849911eecc38468708f660319aa1f
jamie <rmk1075@gmail.com>
2023-02-01 00:43:54 +0900

    feat: add DatasetRepository

---
+++ src/main/java/com/spring/practice/rest/repository/dataset/DatasetRepository.java
@@ -0,0 +1,11 @@
+package com.spring.practice.rest.repository.dataset;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.practice.rest.domain.dataset.Dataset;
+
+@Repository("DatasetRepository")
+public interface DatasetRepository extends JpaRepository<Dataset, Long> {
+
+}
