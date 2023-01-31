ba9a4d42a52f1abab3b89b6f2d9a40878427e0be
jamie <rmk1075@gmail.com>
2023-02-01 01:55:25 +0900

    enhance: add dto, entity mapping method

--- src/main/java/com/spring/practice/rest/common/CommonMapper.java
+++ src/main/java/com/spring/practice/rest/common/CommonMapper.java
@@ -1,8 +1,12 @@
 package com.spring.practice.rest.common;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
+import com.spring.practice.rest.domain.dataset.Dataset;
+import com.spring.practice.rest.domain.dataset.dto.DatasetCreate;
+import com.spring.practice.rest.domain.dataset.dto.DatasetInfo;
 import com.spring.practice.rest.domain.user.User;
 import com.spring.practice.rest.domain.user.dto.UserInfo;
 
@@ -10,4 +14,12 @@ import com.spring.practice.rest.domain.user.dto.UserInfo;
 public interface CommonMapper {
     UserInfo userToUserInfo(User user);
     User userInfoToUser(UserInfo userInfo);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    DatasetInfo datasetCreateToDatasetInfo(DatasetCreate datasetCreate);
+    
+    DatasetInfo datasetToDatasetInfo(Dataset dataset);
+    Dataset datasetInfoToDataset(DatasetInfo dataset);
 }
