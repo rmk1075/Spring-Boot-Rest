b8f47e43a7b5f70a65757b04d844967f951441a7
rmk1075 <rmk1075@gmail.com>
2023-02-02 11:03:54 +0900

    fix: add constructor annotations for dto class

--- src/main/java/com/spring/practice/rest/domain/dataset/dto/DatasetUserCreate.java
+++ src/main/java/com/spring/practice/rest/domain/dataset/dto/DatasetUserCreate.java
@@ -1,10 +1,14 @@
 package com.spring.practice.rest.domain.dataset.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DatasetUserCreate {
 
     private String name;
--- src/main/java/com/spring/practice/rest/domain/user/dto/UserCreate.java
+++ src/main/java/com/spring/practice/rest/domain/user/dto/UserCreate.java
@@ -1,10 +1,14 @@
 package com.spring.practice.rest.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreate {
     
     private String uid;
