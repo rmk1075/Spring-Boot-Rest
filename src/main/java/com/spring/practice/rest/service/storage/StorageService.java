218aef4fc4cf0885760f27ecf758571794f4c7ac
rmk1075 <rmk1075@gmail.com>
2023-02-02 17:37:16 +0900

    feat: create StorageService

--- src/main/java/com/spring/practice/rest/service/storage/StorageService.java
+++ src/main/java/com/spring/practice/rest/service/storage/StorageService.java
@@ -1,11 +1,47 @@
 package com.spring.practice.rest.service.storage;
 
 import java.io.IOException;
-import java.util.List;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface StorageService {
-    public List<String> create(MultipartFile[] files) throws IOException;
-    public void delete(String path) throws IOException;
+import com.spring.practice.rest.service.storage.impl.FileSystemAdapter;
+
+@Service
+public class StorageService {
+
+    @Autowired
+    FileSystemAdapter fileSystemService;
+
+    private final StorageServiceAdapter[] ADAPTERS = new StorageServiceAdapter[]{fileSystemService};
+
+    private StorageServiceAdapter adapter(String url) throws URISyntaxException, IllegalArgumentException {
+        URI uri = new URI(url);
+        String scheme = uri.getScheme();
+        for (StorageServiceAdapter storageServiceAdapter : ADAPTERS) {
+            if(storageServiceAdapter.getScheme().equals(scheme)) {
+                return storageServiceAdapter;
+            }
+        }
+
+        String exception = String.format("Scheme[%s] is not supported. Supported schemes are [%s]",
+            scheme, Arrays.stream(ADAPTERS).map(adpt -> adpt.getScheme()));
+        throw new IllegalArgumentException(exception);
+    }
+
+    public byte[] get(String url) throws IllegalArgumentException, URISyntaxException, IOException {
+        return this.adapter(url).get(url);
+    }
+
+    public String create(String url, byte[] bytes) throws IllegalArgumentException, URISyntaxException, IOException {
+        return this.adapter(url).create(url, bytes);
+    }
+
+    public void delete(String url) throws IllegalArgumentException, URISyntaxException, IOException {
+        this.adapter(url).delete(url);
+    }
+    
 }
