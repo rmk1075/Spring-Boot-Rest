0a4095e046c3ad84386d2e1f999443ac6974dc54
rmk1075 <rmk1075@gmail.com>
2023-02-03 15:44:49 +0900

    feat: add methods to ImageRepository

--- src/main/java/com/spring/practice/rest/repository/image/ImageRepository.java
+++ src/main/java/com/spring/practice/rest/repository/image/ImageRepository.java
@@ -1,5 +1,6 @@
 package com.spring.practice.rest.repository.image;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import com.spring.practice.rest.domain.image.Image;
 @Repository("ImageRepository")
 public interface ImageRepository extends JpaRepository<Image, Long> {
     public Optional<Image> findByName(String name);
+    public List<Image> findAllByDatasetId(Long datasetId);
+    public void deleteAllByDatasetId(Long dataseId);
 }
