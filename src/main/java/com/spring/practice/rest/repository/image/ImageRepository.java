8f7ab618572dadb834a9d032e954bc4db92bec6c
rmk1075 <rmk1075@gmail.com>
2023-02-16 17:27:39 +0900

    feat: add getImageByDataset - apply pagination

--- src/main/java/com/spring/practice/rest/repository/image/ImageRepository.java
+++ src/main/java/com/spring/practice/rest/repository/image/ImageRepository.java
@@ -3,6 +3,8 @@ package com.spring.practice.rest.repository.image;
 import com.spring.practice.rest.domain.image.Image;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,8 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
   public Optional<Image> findByName(String name);
 
   public List<Image> findAllByDatasetId(Long datasetId);
+
+  public List<Image> findAllByDatasetId(Long datasetId, Pageable pageable);
 
   public void deleteAllByDatasetId(Long dataseId);
 }
