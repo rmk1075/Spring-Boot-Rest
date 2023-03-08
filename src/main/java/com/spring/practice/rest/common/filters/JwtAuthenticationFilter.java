a4247391bbaa7d1c055f5fa95a119d250c477897
rmk1075 <rmk1075@gmail.com>
2023-03-08 18:08:59 +0900

    style: apply style tu JwtAuthenticationFilter

--- src/main/java/com/spring/practice/rest/common/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/spring/practice/rest/common/filters/JwtAuthenticationFilter.java
@@ -1,21 +1,21 @@
 package com.spring.practice.rest.common.filters;
 
+import com.spring.practice.rest.common.utils.JwtUtil;
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
-import com.spring.practice.rest.common.utils.JwtUtil;
-
-import lombok.RequiredArgsConstructor;
-
+/**
+ * JwtAuthenticationFilter.
+ * Validate and filtering token from request.
+ */
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
@@ -25,7 +25,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
     String token = jwtUtil.resolveToken((HttpServletRequest) request);
-    if(token != null && jwtUtil.validateToken(token)) {
+    if (token != null && jwtUtil.validateToken(token)) {
       Authentication authentication = jwtUtil.getAuthentication(token);
       SecurityContextHolder.getContext().setAuthentication(authentication);
     }
