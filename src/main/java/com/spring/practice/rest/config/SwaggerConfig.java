54a2203ff4cc048892fec76f3543ae770eb7f3bc
jamie <rmk1075@gmail.com>
2023-03-10 00:49:41 +0900

    feat: add authorization feature to swagger

--- src/main/java/com/spring/practice/rest/config/SwaggerConfig.java
+++ src/main/java/com/spring/practice/rest/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.spring.practice.rest.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
@@ -9,7 +12,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -25,6 +32,8 @@ public class SwaggerConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.OAS_30)
+        .securityContexts(Arrays.asList(securityContext()))
+        .securitySchemes(Arrays.asList(apiKey()))
         .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 등의 노출 여부
         .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
         .select()
@@ -46,6 +55,23 @@ public class SwaggerConfig {
         .description("springboot rest api practice with variable backend tech")
         .version("0.1")
         .build();
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+      .securityReferences(defaultAuthList())
+      .build();
+  }
+
+  private List<SecurityReference> defaultAuthList() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("USER","accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+  }
+
+  private ApiKey apiKey() {
+    return new ApiKey("Authorization", "Authorization", "header");
   }
 }
 
