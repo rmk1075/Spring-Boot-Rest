16e3e5721bda3984ea1d4f51e40102bfaa237eb2
jamie <rmk1075@gmail.com>
2023-06-10 00:52:42 +0900

    refactor: remove unused ModelMapper code

--- src/main/java/com/spring/practice/rest/RestApplication.java
+++ src/main/java/com/spring/practice/rest/RestApplication.java
@@ -1,9 +1,7 @@
 package com.spring.practice.rest;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,10 +10,5 @@ public class RestApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RestApplication.class, args);
-  }
-
-  @Bean
-  public ModelMapper modelMapper() {
-    return new ModelMapper();
   }
 }
--- src/test/java/com/spring/practice/rest/repository/dataset/DatasetRepositoryTest.java
+++ src/test/java/com/spring/practice/rest/repository/dataset/DatasetRepositoryTest.java
@@ -1,61 +1,13 @@
 package com.spring.practice.rest.repository.dataset;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import com.spring.practice.rest.model.dataset.Dataset;
-import com.spring.practice.rest.model.dataset.dto.DatasetCreate;
-
-/**
- * DatasetRepository test code.
- */
+/** DatasetRepository test code. */
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 public class DatasetRepositoryTest {
 
   @Autowired DatasetRepository datasetRepository;
-
-  @Autowired ModelMapper modelMapper;
-
-  @Test
-  void testDatasetRepository() {
-    List<Dataset> datasets = datasetRepository.findAll();
-    assertEquals(0, datasets.size());
-
-    modelMapper
-        .typeMap(DatasetCreate.class, Dataset.class)
-        .addMappings(
-            mapper -> {
-              mapper.map(obj -> null, Dataset::setId);
-            });
-
-    int length = 10;
-    Map<Long, DatasetCreate> map = new HashMap<>();
-    for (int i = 0; i < length; i++) {
-      String name = String.format("dataset-%d", i);
-      String path = String.format("/datasets/%d", i);
-      DatasetCreate datasetCreate = new DatasetCreate(name, path, 0, 0L);
-      Dataset dataset = modelMapper.map(datasetCreate, Dataset.class);
-      dataset = datasetRepository.save(dataset);
-      map.put(dataset.getId(), datasetCreate);
-    }
-
-    datasets = datasetRepository.findAll();
-    assertEquals(datasets.size(), length);
-    for (Dataset dataset : datasets) {
-      Long id = dataset.getId();
-      DatasetCreate datasetCreate = map.get(id);
-      assertEquals(dataset.getName(), datasetCreate.getName());
-      assertEquals(dataset.getPath(), datasetCreate.getPath());
-      assertEquals(dataset.getSize(), datasetCreate.getSize());
-    }
-  }
 }
