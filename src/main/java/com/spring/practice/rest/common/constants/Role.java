0e89d66a154477eae097050fc5e74fe1fa72e35e
jamie <rmk1075@gmail.com>
2023-06-21 01:29:22 +0900

    style: add comments for constant classes - Role, RoleAuthority

--- src/main/java/com/spring/practice/rest/common/constants/Role.java
+++ src/main/java/com/spring/practice/rest/common/constants/Role.java
@@ -1,9 +1,10 @@
 package com.spring.practice.rest.common.constants;
 
-/**
- * User Role Enum.
- */
+/** User Role Enum. */
 public enum Role {
+  /** User Role. */
   USER,
+
+  /** Admin Role. */
   ADMIN,
 }
