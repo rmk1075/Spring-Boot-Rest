720ff620774196a62a58b6c97aeab54b27ff71b8
rmk1075 <rmk1075@gmail.com>
2023-06-22 16:09:55 +0900

    style: adopt code style

--- src/main/java/com/spring/practice/rest/model/BaseEntity.java
+++ src/main/java/com/spring/practice/rest/model/BaseEntity.java
@@ -1,17 +1,17 @@
 package com.spring.practice.rest.model;
 
 import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import lombok.Getter;
-
+/**
+ * Base entity class for all entities.
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
