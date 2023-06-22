f6b07ce32b90650d85610a49f238a8b788a43a1c
rmk1075 <rmk1075@gmail.com>
2023-06-22 17:08:45 +0900

    refactor: UserDb code refactoring

--- src/main/java/com/spring/practice/rest/model/user/dto/UserDb.java
+++ src/main/java/com/spring/practice/rest/model/user/dto/UserDb.java
@@ -2,7 +2,6 @@ package com.spring.practice.rest.model.user.dto;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
  * UserDB dto class.
  * Mapping with User entity.
  */
-@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
