e9aca16ccda466c1a899215cf2d79cc4631e0a57
jamie <rmk1075@gmail.com>
2023-06-24 23:26:17 +0900

    reafctor: modify UserInfo class

--- src/main/java/com/spring/practice/rest/model/user/dto/UserInfo.java
+++ src/main/java/com/spring/practice/rest/model/user/dto/UserInfo.java
@@ -2,14 +2,10 @@ package com.spring.practice.rest.model.user.dto;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * UserInfo dto class.
- */
-@Builder
+/** UserInfo dto class. */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
