ec84249dc1882669560ea6745f8201c5d0b0244a
jamie <rmk1075@gmail.com>
2023-07-03 16:39:25 +0900

    fix: restore createdBy, updatedBy

--- src/main/java/com/spring/practice/rest/model/image/Image.java
+++ src/main/java/com/spring/practice/rest/model/image/Image.java
@@ -1,6 +1,7 @@
 package com.spring.practice.rest.model.image;
 
 import com.spring.practice.rest.model.BaseEntity;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +10,7 @@ import javax.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /** Image Entity class. */
 @Data
@@ -22,8 +24,15 @@ public class Image extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private Long datasetId;
+  @NonNull private Long datasetId;
 
-  private String name;
-  private String url;
+  @NonNull private String name;
+
+  @NonNull private String url;
+
+  @Column(updatable = false)
+  @NonNull
+  private Long createdBy;
+
+  @NonNull private Long updatedBy;
 }
