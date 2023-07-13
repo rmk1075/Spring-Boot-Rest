1e67cb68f58fbea50b15673b557d1c23c4fd8b39
rmk1075 <rmk1075@gmail.com>
2023-07-13 13:31:17 +0900

    style: modify comment

--- src/main/java/com/spring/practice/rest/common/exception/auth/InvalidLoginInfoUnauthenticatedException.java
+++ src/main/java/com/spring/practice/rest/common/exception/auth/InvalidLoginInfoUnauthenticatedException.java
@@ -3,7 +3,10 @@ package com.spring.practice.rest.common.exception.auth;
 import com.spring.practice.rest.common.constant.Message;
 import com.spring.practice.rest.common.exception.base.UnauthenticatedException;
 
-/** InvalidLoginInfoUnauthenticatedException class. Exception for unauthenticated access with invalid login info. */
+/**
+ * InvalidLoginInfoUnauthenticatedException class.
+ * Exception for unauthenticated access with invalid login info.
+ */
 public class InvalidLoginInfoUnauthenticatedException extends UnauthenticatedException {
 
   public InvalidLoginInfoUnauthenticatedException() {
